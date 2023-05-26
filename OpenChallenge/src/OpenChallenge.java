e47618d2758d9ff9ff0ec8bb5fa99b07cef577b8
yhj0318 <gudwns0318@naver.com>
2023-05-26 16:45:44 +0900

    gitTest

--- OpenChallenge/src/OpenChallenge.java
+++ OpenChallenge/src/OpenChallenge.java
@@ -1,9 +1,38 @@
+import java.awt.*;
+import javax.swing.*;
 
-public class OpenChallenge {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello Java");
+class NorthPanel extends JPanel {
+	public NorthPanel() {
+		setBackground(Color.LIGHT_GRAY);
+		setLayout(new FlowLayout());
+		add(new JButton("Open"));
+		add(new JButton("Read"));
+		add(new JButton("Close"));	 
 	}
+}
 
+class CenterPanel extends JPanel {
+	public CenterPanel() {
+		setLayout(null);
+		JLabel la = new JLabel("Hello Java!");
+		la.setSize(100, 20);
+		la.setLocation(100, 50);
+		add(la);
+	}		
+}
+
+public class OpenChallenge extends JFrame {
+	public OpenChallenge() {
+		super("Open Challenge 8");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		add(new NorthPanel(), BorderLayout.NORTH);
+		add(new CenterPanel(), BorderLayout.CENTER);
+		setSize(300, 200);
+		setVisible(true);
+	}
+		
+	public static void main(String[] arg) {
+		new OpenChallenge();
+	}
 }
